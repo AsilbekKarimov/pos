28486d10b41c028e63b84b5be9001b7e2a6385da
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-07-05 18:39:40 +0500

    test

--- src/router/Router.js
+++ src/router/Router.js
@@ -2,14 +2,18 @@ import React, { Suspense, lazy } from "react";
 import { useRoutes, Navigate } from "react-router-dom";
 import Loadable from "../layouts/full/Loadable";
 
-const FiscalModules = Loadable(lazy(() => import("../pages/fiscalModules/FiscalModule")));
-const Application = Loadable(lazy(() => import("../pages/application/Application")));
+const FiscalModules = Loadable(
+  lazy(() => import("../pages/fiscalModules/FiscalModule"))
+);
+const Application = Loadable(
+  lazy(() => import("../pages/application/Application"))
+);
 const Payment = Loadable(lazy(() => import("../pages/fmPayment/Payment")));
 const Reestr = Loadable(lazy(() => import("../pages/reestr/Reestr")));
 
 const RouterConfig = () => {
   const routes = useRoutes([
-    { path: "/", element: <Navigate to="home" /> },
+    { path: "/", element: <Navigate to="modules" /> },
     { path: "modules", element: <FiscalModules /> },
     { path: "application", element: <Application /> },
     { path: "payment", element: <Payment /> },
