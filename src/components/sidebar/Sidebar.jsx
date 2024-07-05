7ffcdb50668d79cd39afdd509489ab2a740830a6
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-07-05 14:49:57 +0500

    Pulling router

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          <ul className="menu bg-base-200 text-base-content min-h-full w-60 p-4">
             <li><a>Главная страница</a></li>
             <li>
               <details>
