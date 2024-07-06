818b7ade1a636e3da5d60f0828dca16dba52a48c
navruzchik-dev <navruzmurodullaev1@gmail.com>
2024-07-06 17:14:22 +0500

    SHoqi gey

--- src/pages/sidebar/Sidebar.jsx
+++ src/pages/sidebar/Sidebar.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 
 const Sidebar = () => {
   return (
-    <div>
-      <div className="drawer lg:drawer-open">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
-          {/* Page content here */}
-        </div>
-        <div className="drawer-side">
-          <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-            <li><a>Главная страница</a></li>
-            <li>
-              <details>
-                <summary>Реестр</summary>
-                <ul>
+    <div className="drawer drawer-mobile">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        {/* Content inside the page */}
+        <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          {/* Sidebar information  */}
+          <li><a>Главная страница</a></li>
+          <li>
+            <details>
+              <summary>Реестр</summary>
+              <ul>
                 <li><a>Фиксальный модули</a></li>
                 <li><a>Торговый точки</a></li>
                 <li><a>Список портнеров ЦТО</a></li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
+              </ul>
+            </details>
+          </li>
+        </ul>
       </div>
     </div>
   );
