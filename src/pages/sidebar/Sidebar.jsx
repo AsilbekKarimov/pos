f012fc431a82f305155337431f113cebdaec09d3
navruzchik-dev <navruzmurodullaev1@gmail.com>
2024-07-09 16:47:50 +0500

    Side

--- src/pages/sidebar/Sidebar.jsx
+++ src/pages/sidebar/Sidebar.jsx
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from './navbar/Navbar';
 
 const Sidebar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="drawer drawer-mobile">
-      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
-        {/* Content inside the page */}
-        <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
-      </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-          {/* Sidebar information  */}
-          <li><a>Главная страница</a></li>
-          <li>
-            <details>
-              <summary>Реестр</summary>
-              <ul>
-                <li><a>Фиксальный модули</a></li>
-                <li><a>Торговый точки</a></li>
-                <li><a>Список портнеров ЦТО</a></li>
-              </ul>
-            </details>
-          </li>
-        </ul>
+    <div>
+      <Navbar toggleSidebar={toggleSidebar} />
+      <div className="drawer drawer-mobile "> {/* добавлен отступ сверху */}
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" checked={isSidebarOpen} readOnly />
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay" onClick={toggleSidebar}></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 pt-16">
+            {/* Sidebar information */}
+            <li className='mt-4'><a>Главная страница</a></li>
+            <li>
+              <details>
+                <summary>Реестр</summary>
+                <ul>
+                  <li><a>Фиксальный модули</a></li>
+                  <li><a>Торговый точки</a></li>
+                  <li><a>Список портнеров ЦТО</a></li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Sidebar;
+export default Sidebar; 
