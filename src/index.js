293ff2fd1927e747f1e2067ebe9f7d115d55a7ed
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-07-11 17:53:58 +0500

    Router bugs is fixed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import './index.css'
-
+import RouterConfig from "./router/Router";
 import { Provider } from "react-redux";
 import store from "./store/store";
-
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+      <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
