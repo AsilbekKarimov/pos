95785007f19784899432184171821c1f67994156
dior011 <diyorraxmatullaev3@gmail.com>
2024-07-24 17:10:21 +0500

    zb

--- src/pages/fiscalModules/FiscalModule.js
+++ src/pages/fiscalModules/FiscalModule.js
@@ -106,7 +106,7 @@ const FiscalModule = () => {
             <Pagination
               currentPage={currentPage}
               totalPages={totalPages}
-              paginate={paginate}
+
               setCurrentPage={setCurrentPage}
             />
           ) : (
