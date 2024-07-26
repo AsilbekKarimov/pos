3f9d2e0ef778c4d6ba818db92fc7218310e33653
dior011 <diyorraxmatullaev3@gmail.com>
2024-07-26 17:00:01 +0500

    d

--- src/others/Button/Button.jsx
+++ src/others/Button/Button.jsx
@@ -1,21 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react'
 
+const Button = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-const Button = ({activeText, NoactiveText}) => {
-    const [active, setActive] = useState(false);
-    
-
-  
-    return (
-        <button
-        className={`mx-auto my-auto py-2 active:scale-90 transition duration-300 flex ${active ? 'bg-primary hover:bg-blue-700' : 'bg-red-500 hover:bg-red-700'} rounded-md text-white px-3`}
-        onClick={()=> setActive(!active) }
-        >
-        {active ? <p>{activeText}</p> : <p>{NoactiveText}</p>}
-        
-
-        </button>
-    );
-};
-
-export default Button;
+export default Button
