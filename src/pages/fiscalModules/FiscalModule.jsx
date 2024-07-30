c62a6a2b6fa372b116c595ce968432334be18a1a
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-07-30 16:17:13 +0500

    partner filter

--- src/pages/fiscalModules/FiscalModule.jsx
+++ src/pages/fiscalModules/FiscalModule.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import useFetch from "../../components/useFetch/useFetch";
 import Pagination from "../../components/pagination/Pagination";
 import FilterRow from "../../components/filterRow/FilterRow";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import Toast from "../../others/toastNotification/Toast";
 
 const FiscalModule = () => {
   const [filters, setFilters] = useState({
@@ -10,20 +13,49 @@ const FiscalModule = () => {
   });
 
   const { data, loading, error } = useFetch("fiscal", "");
+  const token = useSelector((state) => state.auth.accessToken);
+  const userId = useSelector((state) => state.user.user.id);
 
+  const [fiscal, setFiscal] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [rowsPerPage] = useState(10);
+  const isAdmin = useSelector((state) => state.user.user.is_admin)
+  console.log(isAdmin);
+
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get(
+        `https://newterminal.onrender.com/api/fiscal`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200 || response.status === 201) {
+        const data = response.data.filter((obj) => obj.user_id === userId);
+        setFiscal(data);
+      } else {
+        console.log("Error fetching user data");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   useEffect(() => {
     if (data) {
+      fetchUser();
       setFilteredData(data);
     }
   }, [data]);
 
   useEffect(() => {
-    if (data) {
-      const filtered = data.filter((row) => {
+    if (fiscal) {
+      const filtered = fiscal.filter((row) => {
         return (
           row.factory_number
             .toLowerCase()
@@ -36,7 +68,7 @@ const FiscalModule = () => {
       setFilteredData(filtered);
       setCurrentPage(1);
     }
-  }, [filters, data]);
+  }, [filters, fiscal]);
 
   const handleFilterChange = (e) => {
     const { name, value } = e.target;
@@ -63,43 +95,47 @@ const FiscalModule = () => {
       {error && <p>Error: {error.message}</p>}
       {!loading && !error && (
         <div className="flex-grow overflow-y-auto">
-          <table className="table table-md table-zebra border w-full h-full">
-            <thead>
-              <tr className="border font-normal text-[14px] text-blue-700">
-                <th className="border w-16" rowSpan={2}>#</th>
-                <th className="border">Заводской номер кассы</th>
-                <th className="border">Серийный номер фискального модуля</th>
-              </tr>
-              <FilterRow
-                filters={filters}
-                handleFilterChange={handleFilterChange}
-              />
-            </thead>
-            <tbody className="text-[6px]">
-              {currentRows.map((row) => (
-                <tr className="border h-12" key={row.id}>
-                  <th className="border">{row.id}</th>
-                  <td className="border">{row.factory_number}</td>
-                  <td className="border">{row.fiscal_number}</td>
+          {fiscal.length && isAdmin ? (
+            <table className="table table-md table-zebra border w-full h-full">
+              <thead>
+                <tr className="border font-normal text-[14px] text-blue-700">
+                  <th className="border w-16" rowSpan={2}>#</th>
+                  <th className="border">Заводской номер кассы</th>
+                  <th className="border">Серийный номер фискального модуля</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+                <FilterRow
+                  filters={filters}
+                  handleFilterChange={handleFilterChange}
+                />
+              </thead>
+              <tbody className="text-[6px]">
+                {currentRows.map((row, index) => (
+                  <tr className="border h-12" key={row.id}>
+                    <th className="border">{index + 1 + indexOfFirstRow}</th>
+                    <td className="border">{row.factory_number}</td>
+                    <td className="border">{row.fiscal_number}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <div>
+              <Toast message="У вас не имеется фискальных модулей!" error={true} />
+              <p className="flex justify-center items-center h-screen text-xl font-semibold text-red-600">Нет данных</p>
+            </div>
+          )}
         </div>
       )}
-      {filteredData.length > rowsPerPage ? (
+      {filteredData.length > rowsPerPage && (
         <Pagination
           currentPage={currentPage}
           totalPages={totalPages}
           paginate={paginate}
           setCurrentPage={setCurrentPage}
         />
-      ) : (
-        <div></div>
       )}
     </div>
   );
 };
 
 export default FiscalModule;
-
